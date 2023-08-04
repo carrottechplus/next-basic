fe6c1d642befe06b332690576676c233a01a88da
carrottechplus <blohzzang@gmail.com>
2023-08-04 12:56:54 +0900

    isr 방식의 이해

--- pages/isr.jsx
+++ pages/isr.jsx
@@ -1,11 +1,21 @@
 import SubLayout from '@/components/SubLayout';
 
-function Isr() {
+function Isr(props) {
 	return (
 		<SubLayout name={'ISR'}>
 			<p>ISR방식</p>
+			<h1>{props.now}</h1>
 		</SubLayout>
 	);
+}
+
+export async function getStaticProps() {
+	console.log('isr');
+
+	return {
+		props: { now: performance.now() },
+		revalidate: 5,
+	};
 }
 
 export default Isr;
