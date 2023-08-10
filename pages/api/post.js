6a4d5813ce738886870440c02b5c76ba961e739f
carrottechplus <blohzzang@gmail.com>
2023-08-10 11:10:16 +0900

    데이터 저장 작업 흐름 설명

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,15 +1,22 @@
 import { connectMongoDB } from '@/libs/MongoConnect';
 
-// 안쪽에서 await 로 동기화 처리할 예정이므로 wrapping 함수에 async 지정
 export default async function handler(req, res) {
-	// await 로 동기화할거라서 then, catch 문을 쓸 수 없음, try, catch로 예외처리
 	try {
 		// 요청 성공 시
 		const DB = await connectMongoDB();
-		console.log(DB);
-		res.status(200).send('success');
+		// res.status(200).send('success');
 	} catch (err) {
 		// 요청 실패 시
 		res.status(400).send({ err });
 	}
 }
+
+/*
+데이터 저장 작업 순서
+1. 저장할 데이터의 구조에 맞게 스키마 생성
+2. api라우터에서 스키마 적용된 모델객체에 클라이언트로부터 전달받은 데이터를 바인딩 후, DB에 저장. 그 후 응답 성공 클라이언트로 전송
+2-1. 클라이언트에서 post 요청 받음.
+2-2. 카운터모델에서 communityNum 값을 가져온 뒤, 클라이언트로 전달받은 객체와 결합
+2-3. 결합된 객체를 Post모델로 저장
+2-4. 저장이 완료되면 다시 Counter값을 1씩 증가
+*/
