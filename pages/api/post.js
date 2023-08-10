37e4fc71e33e30aaf2ba463ca9e08e5b65e4eb46
carrottechplus <blohzzang@gmail.com>
2023-08-10 11:41:08 +0900

    DB에 글 저장  요청 라우터 설정

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,13 +1,40 @@
 import { connectMongoDB } from '@/libs/MongoConnect';
+import { Community } from '@/model/CommunitySchema';
+import { Counter } from '@/model/CounterSchema';
 
 export default async function handler(req, res) {
-	try {
-		// 요청 성공 시
-		const DB = await connectMongoDB();
-		// res.status(200).send('success');
-	} catch (err) {
-		// 요청 실패 시
-		res.status(400).send({ err });
+	// 전달된 요청 방식이 POST일 때 처리 (글 저장)
+	if (req.method === 'POST') {
+		// 클라이언트로부터 전달받은 데이터 정보 {title, counter}
+		const temp = req.body;
+
+		try {
+			// 요청 성공 시
+			await connectMongoDB();
+			// res.status(200).send('success');
+
+			Counter.findOne({ name: 'counter' })
+				.exec()
+				.then((doc) => {
+					// 카운터 모델에서 가져온 고유번호를 클라이언트에서 넘어온 데이터에 추가
+					temp.communityNum = doc.communityNum;
+					// 위에서 결합된 객체를 Community Model객체로 DB에 저장
+					const CommunityModel = new Community(temp);
+					CommunityModel.save().then(() => {
+						// 글 저장이 완료되면 Counter 모델의 communityNum값을 1 증가
+						Counter.updateOne({ name: 'counter' }, { $inc: { communityNum: 1 } })
+							.exec()
+							.then(() => {
+								// 카운터 정보값도 갱신 완료되면, 클라이언트 쪽에 저장 성공 응답 전달
+								res.json({ success: true });
+							})
+							.catch((err) => res.json({ success: false, err: err }));
+					});
+				});
+		} catch (err) {
+			// 요청 실패 시
+			res.status(400).send({ err });
+		}
 	}
 }
 
